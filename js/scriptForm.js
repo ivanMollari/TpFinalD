9e4531e28ce5308dd86e00a65520f853b0f4c5a7
ivanMollari <ivanmollari.96@gmail.com>
2021-07-11 21:21:35 -0300

    Cambio en form signIng/signUp mas responsive

--- js/scriptForm.js
+++ js/scriptForm.js
@@ -1,5 +1,7 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
+const signUpButtonMov = document.getElementById('signUpMov');
+const signInButtonMov = document.getElementById('signInMov');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
@@ -8,4 +10,12 @@ signUpButton.addEventListener('click', () => {
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+});
+
+signUpButtonMov.addEventListener('click', () => {
+	container.classList.add("up-panel-active");
+});
+
+signInButtonMov.addEventListener('click', () => {
+	container.classList.remove("up-panel-active");
 });
